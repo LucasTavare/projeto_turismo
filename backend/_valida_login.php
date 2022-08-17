28f62003557059c1eb24756ce2c934dff4e13194
Lucas Protestato Tavares <101677196+LucasTavare@users.noreply.github.com>
2022-08-17 20:02:23 -0300

    login finalizado

--- backend/_valida_login.php
+++ backend/_valida_login.php
@@ -2,19 +2,30 @@
 include ('../backend/conexao.php');
 
 try{
-    $usuario = $_POST('email');
+    $usuario = $_POST['login'];
 
-    $senha = $_POST('senha');
+    $senha = $_POST['senha'];
 
     $sql = "SELECT * FROM tb_login WHERE email = '$usuario' AND senha = '$senha';";
 
-    $comando = $conexao->prepare($sql);
+    $comando = $con->prepare($sql);
 
     $comando -> execute();
 
     $dados = $comando->fetchALL(PDO::FETCH_ASSOC);
 
-    var_dump($dados);
+    // var_dump($dados);
+
+    // verifica se existem registros dentro da variavel dados
+
+    if($dados != null){
+
+
+        header('location: ../admin/gerenciar-viagens.php');
+    }else{
+        header('location: ../admin/index.html');
+        echo "usuario ou senha invalido";
+    }
 
 
 }catch(PDOException $erro){
